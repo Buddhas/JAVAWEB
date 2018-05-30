f68cf3ed77f8f441b0ff20b55db829b1a245868b
Buddhas <2424932515@qq.com>
2018-05-30 11:35:27 +0800

    过滤器解决登录验证

---
+++ servletFilter/src/com/servletFilter/loginFilter.java
@@ -0,0 +1,59 @@
+package com.servletFilter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class logFilter
+ */
+@WebFilter("/loginFilter")
+public class loginFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public loginFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		HttpServletResponse response = (HttpServletResponse)servletResponse;
+		HttpSession session = request.getSession();
+		if(session.getAttribute("userName") !=null){
+			//System.out.println("我拦截了");
+			chain.doFilter(request, response);
+		}else{
+			System.out.println("我拦截了");
+			response.sendRedirect("/servletFilter/login.jsp");
+		}
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ servletFilter/src/com/servletLogin/login.java
@@ -0,0 +1,48 @@
+package com.servletLogin;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet("/login")
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
+		HttpSession session = request.getSession();
+		if(userName.equals("笑佛弥勒") && password.equals("19970209")){
+			session.setAttribute("userName", userName);
+			session.setAttribute("password", password);
+			response.sendRedirect("/servletFilter/success.jsp");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
